4e628e83d1ec3ca7e8cc4bd2a2f22d6a71d82bac
Francisco Soares <fmssn@cin.ufpe.br>
2016-09-12 17:49:47 -0300

    Implementada complicação.
    Implementada a complicação. Exemplo simples, apenas com Utilitarian Large e Modular Large. Inclui viagem no tempo.

--- RelogioExemplo/RelogioExemplo WatchKit Extension/ComplicationController.swift
+++ RelogioExemplo/RelogioExemplo WatchKit Extension/ComplicationController.swift
@@ -14,15 +14,17 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirectionsForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTimeTravelDirections) -> Void) {
+        //funciona para trás ou para a frente.
         handler([.Forward, .Backward])
     }
     
     func getTimelineStartDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
-        handler(nil)
+        //        handler(NSDate(timeInterval: -(60*60*24), sinceDate: NSDate()))
+        handler(NSDate(timeIntervalSinceNow: -60*60*24))
     }
     
     func getTimelineEndDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
-        handler(nil)
+        handler(NSDate(timeIntervalSinceNow: 24*60*60))
     }
     
     func getPrivacyBehaviorForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationPrivacyBehavior) -> Void) {
@@ -33,31 +35,173 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntryForComplication(complication: CLKComplication, withHandler handler: ((CLKComplicationTimelineEntry?) -> Void)) {
         // Call the handler with the current timeline entry
-        handler(nil)
+        
+        
+        var entry : CLKComplicationTimelineEntry? = nil
+        
+        
+        let textProvider = CLKSimpleTextProvider(text: "12 bruffles", shortText: "12bruf")
+        
+        if complication.family == .ModularLarge {
+            
+            let lf = CLKComplicationTemplateModularLargeStandardBody()
+            
+            lf.headerTextProvider = textProvider
+            lf.body1TextProvider = CLKSimpleTextProvider(text: "relógio exemplo")
+            lf.body2TextProvider = CLKSimpleTextProvider(text: "5 draffles", shortText: "5draf")
+            
+            entry = CLKComplicationTimelineEntry(date: NSDate(), complicationTemplate: lf)
+        }
+        
+        if complication.family == .UtilitarianLarge {
+            let a : CLKComplicationTemplateUtilitarianLargeFlat =
+                CLKComplicationTemplateUtilitarianLargeFlat()
+            a.textProvider = textProvider
+            
+            entry = CLKComplicationTimelineEntry(date: NSDate(), complicationTemplate: a)
+        }
+        
+        handler(entry)
     }
     
     func getTimelineEntriesForComplication(complication: CLKComplication, beforeDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
         // Call the handler with the timeline entries prior to the given date
-        handler(nil)
+        
+        var entries : [CLKComplicationTimelineEntry]? = nil
+        
+        if complication.family == .ModularLarge {
+            entries = []
+            
+            for i : Int in 1...limit {
+                let newDate = date.dateByAddingTimeInterval(-Double(i)*10*60)
+                let lf = CLKComplicationTemplateModularLargeStandardBody()
+                
+                let numero = 12 - i
+                
+                let textProvider = CLKSimpleTextProvider(text: "\(numero) bruffles", shortText: "\(numero)bruf")
+                
+                lf.headerTextProvider = textProvider
+                lf.body1TextProvider = CLKSimpleTextProvider(text: "relógio exemplo")
+                lf.body2TextProvider = CLKSimpleTextProvider(text: "5 draffles", shortText: "5 draf")
+                
+                entries?.append(CLKComplicationTimelineEntry(date: newDate, complicationTemplate: lf))
+            }
+            
+            //            entries = []
+        }
+        
+        if complication.family == .UtilitarianLarge {
+            entries = []
+            
+            for i : Int in 1...limit {
+                let newDate = date.dateByAddingTimeInterval(-Double(i)*10*60)
+                let a : CLKComplicationTemplateUtilitarianLargeFlat =
+                    CLKComplicationTemplateUtilitarianLargeFlat()
+                let numero = 12 - i
+                
+                let textProvider = CLKSimpleTextProvider(text: "\(numero) bruffles", shortText: "\(numero)bruf")
+                
+                a.textProvider = textProvider
+                
+                entries?.append(CLKComplicationTimelineEntry(date: newDate, complicationTemplate: a))
+            }
+            
+        }
+        
+        handler(entries?.reverse())
+        
+        
+        //        handler(nil)
     }
     
     func getTimelineEntriesForComplication(complication: CLKComplication, afterDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
         // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        //        handler(nil)
+        var entries : [CLKComplicationTimelineEntry]? = nil
+        
+        if complication.family == .ModularLarge {
+            entries = []
+            
+            for i : Int in 1...limit {
+                let newDate = date.dateByAddingTimeInterval(Double(i)*10*60)
+                let lf = CLKComplicationTemplateModularLargeStandardBody()
+                
+                let numero = 12 + i
+                
+                let textProvider = CLKSimpleTextProvider(text: "\(numero) bruffles", shortText: "\(numero)bruf")
+                
+                lf.headerTextProvider = textProvider
+                lf.body1TextProvider = CLKSimpleTextProvider(text: "relógio exemplo")
+                lf.body2TextProvider = CLKSimpleTextProvider(text: "5 draffles", shortText: "5draf")
+                
+                entries?.append(CLKComplicationTimelineEntry(date: newDate, complicationTemplate: lf))
+            }
+            
+            //            entries = []
+        }
+        
+        if complication.family == .UtilitarianLarge {
+            entries = []
+            
+            for i : Int in 1...limit {
+                let newDate = date.dateByAddingTimeInterval(Double(i)*10*60)
+                let a : CLKComplicationTemplateUtilitarianLargeFlat =
+                    CLKComplicationTemplateUtilitarianLargeFlat()
+                let numero = 12 + i
+                
+                let textProvider = CLKSimpleTextProvider(text: "\(numero) bruffles", shortText: "\(numero)bruf")
+                
+                a.textProvider = textProvider
+                
+                entries?.append(CLKComplicationTimelineEntry(date: newDate, complicationTemplate: a))
+            }
+            
+        }
+        
+        handler(entries)
     }
     
     // MARK: - Update Scheduling
     
     func getNextRequestedUpdateDateWithHandler(handler: (NSDate?) -> Void) {
         // Call the handler with the date when you would next like to be given the opportunity to update your complication content
-        handler(nil);
+        
+        handler(NSDate(timeIntervalSinceNow: 10*60))//;
     }
     
     // MARK: - Placeholder Templates
     
     func getPlaceholderTemplateForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        //handler(nil)
+        
+        
+        var goThings : CLKComplicationTemplate? = nil
+        if complication.family == .UtilitarianLarge {
+            
+            
+            let a : CLKComplicationTemplateUtilitarianLargeFlat =
+                CLKComplicationTemplateUtilitarianLargeFlat()
+            a.textProvider = CLKSimpleTextProvider(text: "Existem 12 bruffles", shortText: "12 bruf")
+            
+            goThings = a
+            
+        }
+        
+        if complication.family == .ModularLarge {
+            let setimaB = CLKComplicationTemplateModularLargeStandardBody()
+            
+            setimaB.headerTextProvider = CLKSimpleTextProvider(text: "2 bruffles")
+            setimaB.body1TextProvider = CLKSimpleTextProvider(text: "Relógio exemplo")
+            setimaB.body2TextProvider = CLKSimpleTextProvider(text: "0 draffles")
+            //            handler(setimaB)
+            goThings = setimaB
+        }
+        
+        
+        
+        handler(goThings)
+        
     }
     
 }
